5b4d6ca0163c43d2f577aad39bfdb292ec489578
lemongautam79 <lemongautam79@gmail.com>
2025-08-22 19:28:28 +0545

    [Scalar Configuration]

--- ASPNETCoreWebAPI/Program.cs
+++ ASPNETCoreWebAPI/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,6 +13,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    //Scalar Setup
+    app.MapScalarApiReference();
     app.MapOpenApi();
 }
 
