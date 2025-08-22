e6f9bf5a5026bc4dcecb0c5e3c020786a83faeae
lemongautam79 <lemongautam79@gmail.com>
2025-08-22 20:41:35 +0545

    [Employee Static Storage]

---
+++ ASPNETCoreWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreWebAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(
+                new
+                {
+                    Message = "Welcome to ASP.NET Core Web API 🚀",
+                    Status = "Running"
+                }
+                );
+        }
+    }
+}
